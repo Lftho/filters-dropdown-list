7eb4da71afb562481c0c02483eadd2cae41edd79
Luiz Fernando Thomaz <lft.thomaz@gmail.com>
2022-07-11 14:43:34 -0300

    feat: filtrando a partir do search e o dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
+import { ServiceService } from './service.service';
 import { Component } from '@angular/core';
-
+import { MatTableDataSource } from '@angular/material/table';
+import * as _ from 'lodash';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'filters-dropdown-list';
+
+  displayedColumns = ['name', 'username', 'email', 'gender'];
+  dataSource!: MatTableDataSource<any>;
+  apiResponse: any = [];
+  genderOptions: any = [];
+
+  constructor(private service: ServiceService) { }
+
+  ngOnInit() {
+    this.service.getUserData().subscribe((response: any) => {
+      this.apiResponse = response;
+      this.genderOptions = _.uniq(response.map((item: any) => item['gender'])) //dados unico sendo exibido dentro do dropdown
+      this.dataSource = new MatTableDataSource(response);
+      console.log('Itens do dropdown ', this.genderOptions)
+    })
+  }
+
+  filterData($event: any) {
+    this.dataSource.filter = $event.target.value;
+    console.log('FilterData', this.dataSource.filter)
+  }
+
+  changeFilter($event: any) {
+    let filteredData = _.filter(this.apiResponse, (item) => {
+      console.log('Filter', item)
+      return item.gender == $event.value;
+    })
+    this.dataSource = new MatTableDataSource(filteredData);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { ServiceService } from './service.service';
+
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +21,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
